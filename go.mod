978a4e904d3a8a1b885243b406002579a76f1b90
Oleksandr Redko <oleksandr.red+github@gmail.com>
2021-03-12 22:50:06 +0200

    Try participle expression parser

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/alexandear/scalc
 
 go 1.16
 
-require github.com/golangci/golangci-lint v1.38.0
+require (
+	github.com/alecthomas/participle/v2 v2.0.0-alpha3
+	github.com/golangci/golangci-lint v1.38.0
+)
--- main.go
+++ main.go
@@ -2,8 +2,70 @@ package main
 
 import (
 	"log"
+
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer/stateful"
 )
 
+type OpType string
+
+const (
+	OpEQ OpType = "EQ"
+	OpLE OpType = "LE"
+	OpGR OpType = "GR"
+)
+
+func LexerOperator() string {
+	operations := []OpType{OpEQ, OpLE, OpGR}
+
+	var res string
+	for i, op := range operations {
+		res += string(op)
+
+		if i != len(operations)-1 {
+			res += "|"
+		}
+	}
+
+	return res
+}
+
+type Expression struct {
+	Set Set `"[" @@+ "]"`
+}
+
+type N struct {
+	PositiveNumber int `@PositiveNumber`
+}
+
+type File struct {
+	File string `@File`
+}
+
+type Operation struct {
+	Type OpType `@Operator`
+}
+
+type Set struct {
+	Operation Operation `@@`
+	N         N         `@@`
+	Files     []File    `@@+`
+}
+
 func main() {
-	log.Println("Sets calculator")
+	l := stateful.MustSimple([]stateful.Rule{
+		{"PositiveNumber", `[0-9]\d*`, nil},
+		{"File", `[a-zA-Z]\.\w*`, nil},
+		{"Operator", LexerOperator(), nil},
+		{"Bracket", `\[|\]`, nil},
+		{"Whitespace", `[ \t\n\r]+`, nil},
+	})
+	parser := participle.MustBuild(&Expression{}, participle.Lexer(l), participle.Elide("Whitespace"))
+
+	out := &Expression{}
+	if err := parser.ParseString("", `[LE 2 d.txt [ GR 2 a.txt b.txt c.txt ] ]`, out); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%+v", *out)
 }
